5b1331fea3a2416e6108e88753093f3e06e020b4
Álex B <Leex@protonmail.com>
2020-11-12 00:25:20 +0100

    Update Watson.cs
    Main class update

--- winPEAS/winPEASexe/winPEAS/Watson.cs
+++ winPEAS/winPEASexe/winPEAS/Watson.cs
@@ -1004,49 +1004,53 @@ namespace winPEAS
     //////////////////////////////
     class Watson
     {
-        public static void FindVulns()
+        static void Main(string[] args)
         {
-            System.Console.WriteLine(Beaprint.YELLOW + "  [?] " + Beaprint.LBLUE + "Windows vulns search powered by " + Beaprint.LRED + "Watson" + Beaprint.LBLUE + "(https://github.com/rasta-mouse/Watson)" + Beaprint.NOCOLOR);
+            Info.PrintLogo();
 
             // Supported versions
-            List<string> supportedVersions = new List<string>()
+            var supportedVersions = new Dictionary<int, string>()
             {
-                "10240", //1507
-                "10586", //1511
-                "14393", //1607 & 2K16
-                "15063", //1703
-                "16299", //1709
-                "17134", //1803
-                "17763", //1809 & 2K19
-                "18362", //1903
-                "18363", //1909
+                { 10240, "1507" }, { 10586, "1511" }, { 14393, "1607" }, { 15063, "1703" }, { 16299, "1709" },
+                { 17134, "1803" }, { 17763, "1809" }, { 18362, "1903" }, { 18363, "1909" }, { 19041, "2004" }
             };
 
             // Get OS Build number
-            string buildNumber = Wmi.GetBuildNumber();
-            if (!string.IsNullOrEmpty(buildNumber))
-                System.Console.WriteLine(String.Format("    {0}: {1}", "OS Build Number", buildNumber));
-            else
-                return;
-
-            if (!supportedVersions.Contains(buildNumber))
+            var buildNumber = Wmi.GetBuildNumber();
+            if (buildNumber != 0)
             {
-                Beaprint.GoodPrint("   Windows version not supported\r\n");
+                var version = supportedVersions[buildNumber];
+                Console.WriteLine(" [*] OS Version: {0} ({1})", version, buildNumber);
+            }
+            else
+            {
+                Console.Error.WriteLine(" [!] Could not retrieve Windows BuildNumber");
+                return;
+            }   
+
+            if (!supportedVersions.ContainsKey(buildNumber))
+            {
+                Console.Error.WriteLine(" [!] Windows version not supported");
                 return;
             }
 
             // List of KBs installed
-            //Console.WriteLine(" [*] Enumerating installed KBs...\r\n");
-            List<string> installedKBs = Wmi.GetInstalledKBs();
+            Console.WriteLine(" [*] Enumerating installed KBs...");
+            var installedKBs = Wmi.GetInstalledKBs();
 
-            /*#if DEBUG
-                        foreach (string kb in installedKBs)
-                            Console.WriteLine(" {0}", kb);
-                        Console.WriteLine();
-            #endif*/
+#if DEBUG
+            Console.WriteLine();
+
+            foreach (var kb in installedKBs)
+            {
+                Console.WriteLine(" {0}", kb);
+            }
+                
+            Console.WriteLine();
+#endif
 
             // List of Vulnerabilities
-            VulnerabilityCollection vulnerabiltiies = new VulnerabilityCollection();
+            var vulnerabiltiies = new VulnerabilityCollection();
 
             // Check each one
             CVE_2019_0836.Check(vulnerabiltiies, buildNumber, installedKBs);
@@ -1058,6 +1062,9 @@ namespace winPEAS
             CVE_2019_1385.Check(vulnerabiltiies, buildNumber, installedKBs);
             CVE_2019_1388.Check(vulnerabiltiies, buildNumber, installedKBs);
             CVE_2019_1405.Check(vulnerabiltiies, buildNumber, installedKBs);
+            CVE_2020_0668.Check(vulnerabiltiies, buildNumber, installedKBs);
+            CVE_2020_0683.Check(vulnerabiltiies, buildNumber, installedKBs);
+            CVE_2020_1013.Check(vulnerabiltiies, buildNumber, installedKBs);
 
             // Print the results
             vulnerabiltiies.ShowResults();
