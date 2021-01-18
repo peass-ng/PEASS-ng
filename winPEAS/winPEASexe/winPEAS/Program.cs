a27ad668925fe9052b41f78131b1ba8767300c60
makikvues <makikvues2@gmail.com>
2021-01-18 23:37:55 +0100

    - Program and Main made public

--- winPEAS/winPEASexe/winPEAS/Program.cs
+++ winPEAS/winPEASexe/winPEAS/Program.cs
@@ -2,13 +2,13 @@
 
 namespace winPEAS
 {
-    class Program
+    public static class Program
     {
         // Static blacklists        
         //static string goodSoft = "Windows Phone Kits|Windows Kits|Windows Defender|Windows Mail|Windows Media Player|Windows Multimedia Platform|windows nt|Windows Photo Viewer|Windows Portable Devices|Windows Security|Windows Sidebar|WindowsApps|WindowsPowerShell| Windows$|Microsoft|WOW6432Node|internet explorer|Internet Explorer|Common Files";                       
 
         [STAThread]
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Checks.Checks.Run(args);
         }
