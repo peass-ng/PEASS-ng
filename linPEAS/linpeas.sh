6e091377950dbc9519bbd621256e3bfe33ec5cb3
Carlos Polop <carlospolop@gmail.com>
2021-04-09 10:05:38 +0100

    Update linpeas.sh

--- linPEAS/linpeas.sh
+++ linPEAS/linpeas.sh
@@ -1,4 +1,4 @@
-f#!/bin/sh
+#!/bin/sh
 
 VERSION="v3.1.4"
 ADVISORY="This script should be used for authorized penetration testing and/or educational purposes only. Any misuse of this software will not be the responsibility of the author or of any other collaborator. Use it at your own networks and/or with the network owner's permission."
