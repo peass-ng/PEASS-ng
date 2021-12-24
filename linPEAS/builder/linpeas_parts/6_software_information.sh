bc328b1163905a5b82f07f8b2a5c0eed48be511f
carlospolop <carlospolop@gmail.com>
2021-12-23 20:41:15 -0500

    log4j

--- linPEAS/builder/linpeas_parts/6_software_information.sh
+++ linPEAS/builder/linpeas_parts/6_software_information.sh
@@ -181,7 +181,7 @@ peass{Ldap}
 if [ "$PSTORAGE_LOG4SHELL" ] || [ "$DEBUG" ]; then
   print_2title "Searching Log4Shell vulnerable libraries"
   printf "%s\n" "$PSTORAGE_LOG4SHELL" | while read f; do
-    echo "$f" | grep -E "log4j\-core\-(1\.|2\.[0-9][^0-9]|2\.1[0-6])" | sed -${E} "s,log4j\-core\-(1\.|2\.[0-9][^0-9]|2\.1[0-6]),${SED_RED},";
+    echo "$f" | grep -E "log4j\-core\-(1\.[^0]|2\.[0-9][^0-9]|2\.1[0-6])" | sed -${E} "s,log4j\-core\-(1\.[^0]|2\.[0-9][^0-9]|2\.1[0-6]),${SED_RED},";
   done
   echo ""
 fi
