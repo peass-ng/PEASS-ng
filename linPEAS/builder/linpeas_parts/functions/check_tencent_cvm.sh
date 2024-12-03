ae49e7fa0232391009ec51821bf810923407965c
Carlos Polop <carlospolop@gmail.com>
2024-12-03 19:51:16 +0100

    fix tencent

--- linPEAS/builder/linpeas_parts/functions/check_tencent_cvm.sh
+++ linPEAS/builder/linpeas_parts/functions/check_tencent_cvm.sh
@@ -16,7 +16,7 @@
 
 check_tencent_cvm () {
   is_tencent_cvm="No"
-  if [ -f "/etc/cloud/cloud.cfg.d/05_logging.cfg" ] || grep -qi Tencent /etc/cloud/cloud.cfg; then
+  if grep -qi Tencent /etc/cloud/cloud.cfg; then
       is_tencent_cvm="Yes"
   fi
 }
