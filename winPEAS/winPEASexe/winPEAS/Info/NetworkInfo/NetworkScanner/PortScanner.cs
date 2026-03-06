db00c35f51fc27433271f845e7f23acd900168ec
giveen <ajaxx20020@gmail.com>
2026-03-06 12:20:08 -0700

    Remove unused System.Threading.Tasks import from PortScanner

--- winPEAS/winPEASexe/winPEAS/Info/NetworkInfo/NetworkScanner/PortScanner.cs
+++ winPEAS/winPEASexe/winPEAS/Info/NetworkInfo/NetworkScanner/PortScanner.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Threading;
-using System.Threading.Tasks;
 using winPEAS.Helpers;
 
 namespace winPEAS.Info.NetworkInfo.NetworkScanner
