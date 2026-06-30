5cab04cf53e329ab4aa694cddc46a0241c23044e
HackTricks PEASS Autoimprover <peass-autoimprover@hacktricks.xyz>
2026-06-30 06:16:02 +0000

    autoimprover: simplify winpeas checks

--- winPEAS/winPEASexe/winPEAS/Info/ApplicationInfo/InstalledPackageInventory.cs
+++ winPEAS/winPEASexe/winPEAS/Info/ApplicationInfo/InstalledPackageInventory.cs
@@ -1,7 +1,6 @@
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace winPEAS.Info.ApplicationInfo
 {
@@ -57,30 +56,16 @@ namespace winPEAS.Info.ApplicationInfo
                                 continue;
                             }
 
-                            var name = CleanValue(appKey.GetValue("DisplayName"));
-                            var version = CleanValue(appKey.GetValue("DisplayVersion"));
-                            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(version))
+                            var package = CreatePackage(appKey);
+                            if (package == null)
                             {
                                 continue;
                             }
 
-                            var key = $"{name}\0{version}";
+                            var key = $"{package["name"]}\0{package["version"]}";
                             if (!seen.Add(key))
                             {
                                 continue;
-                            }
-
-                            var package = new Dictionary<string, string>
-                            {
-                                { "name", name },
-                                { "version", version },
-                                { "manager", "windows-registry" }
-                            };
-
-                            var publisher = CleanValue(appKey.GetValue("Publisher"));
-                            if (!string.IsNullOrWhiteSpace(publisher))
-                            {
-                                package["publisher"] = publisher;
                             }
 
                             packages.Add(package);
@@ -92,6 +77,31 @@ namespace winPEAS.Info.ApplicationInfo
             {
                 // Registry inventory is best-effort; unreadable views should not break winPEAS.
             }
+        }
+
+        private static Dictionary<string, string> CreatePackage(RegistryKey appKey)
+        {
+            var name = CleanValue(appKey.GetValue("DisplayName"));
+            var version = CleanValue(appKey.GetValue("DisplayVersion"));
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(version))
+            {
+                return null;
+            }
+
+            var package = new Dictionary<string, string>
+            {
+                { "name", name },
+                { "version", version },
+                { "manager", "windows-registry" }
+            };
+
+            var publisher = CleanValue(appKey.GetValue("Publisher"));
+            if (!string.IsNullOrWhiteSpace(publisher))
+            {
+                package["publisher"] = publisher;
+            }
+
+            return package;
         }
 
         private static string CleanValue(object value)
