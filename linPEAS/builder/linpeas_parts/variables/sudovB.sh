a784f533c1028819297e567566d378da503fdb1c
HackTricks PEASS Wordlist Updater <peass-wordlist-updater@hacktricks.xyz>
2026-08-03 06:40:11 +0000

    wordlist-updater: refresh linpeas vulnerability lists

--- linPEAS/builder/linpeas_parts/variables/sudovB.sh
+++ linPEAS/builder/linpeas_parts/variables/sudovB.sh
@@ -1,7 +1,7 @@
 # Title: Variables - sudovB
 # ID: sudovB
 # Author: Carlos Polop
-# Last Update: 26-02-2026
+# Last Update: 03-08-2026
 # Description: Sudo version bad regex
 # License: GNU GPL
 # Version: 1.0
@@ -13,4 +13,4 @@
 # Small linpeas: 1
 
 
-sudovB="[01].[012345678].[0-9]+|1.9.[01234][^0-9]|1.9.[01234]$|1.9.5p1|1\.9\.[6-9]|1\.9\.1[0-6]|1\.9\.17([^p]|$)"
+sudovB="[01].[012345678].[0-9]+|1.9.[01234][^0-9]|1.9.[01234]$|1.9.5p1|1\.9\.[6-9]|1\.9\.1[0-6]|1\.9\.17($|[^0-9p]|p[12]([^0-9]|$))"
